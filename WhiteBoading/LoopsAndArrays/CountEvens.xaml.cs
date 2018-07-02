6ff7c7802c029a890b681b9672472cb22ffc169a
Nabiel El-Gendy <stryphhh@gmail.com>
2018-07-01 23:28:14 -0400

    first commit

---
+++ WhiteBoading/LoopsAndArrays/CountEvens.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhiteBoading.LoopsAndArrays
+{
+    /// <summary>
+    /// Interaction logic for CountEvens.xaml
+    /// </summary>
+    public partial class CountEvens : Window
+    {
+        public CountEvens()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CountEvensAnswer answer = new CountEvensAnswer();
+            answer.ShowDialog();
+        }
+    }
+}
